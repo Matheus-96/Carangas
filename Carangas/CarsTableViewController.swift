7fbd06130362d93eb014c36808ce50a0ecef8fd2
Matheus Rodrigues Araujo <matheusaraujo@NBBV016619.BRQ.COM>
2020-01-07 13:51:16 -0300

    CRUD completo

---
+++ Carangas/CarsTableViewController.swift
@@ -0,0 +1,117 @@
+//
+//  CarsTableViewController.swift
+//  Carangas
+//
+//  Created by Eric Brito on 21/10/17.
+//  Copyright © 2017 Eric Brito. All rights reserved.
+//
+
+import UIKit
+
+class CarsTableViewController: UITableViewController {
+    
+    var cars: [Car] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        REST.loadCars(onComplete: { (cars) in
+            self.cars = cars
+            DispatchQueue.main.sync {
+                self.tableView.reloadData()
+            }
+        }) { (error) in
+            print(error)
+//            switch error {
+//            case .invalidJSON
+//                print("JSON Inválido")
+//            case .noData
+//                print("Sem nenhum JSON")
+//            }
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return cars.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        let car = cars[indexPath.row]
+        cell.textLabel?.text = car.name
+        cell.detailTextLabel?.text = car.brand
+
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if editingStyle == .delete {
+            let car = cars[indexPath.row]
+            REST.delete(car: car) { (sucess) in
+                if sucess {
+                    self.cars.remove(at: indexPath.row)
+                    DispatchQueue.main.async {
+                        tableView.deleteRows(at: [indexPath], with: .fade)
+                    }
+                }
+            }
+            
+        }
+    }
+    
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "viewSegue" {
+            let vc = segue.destination as! CarViewController
+            vc.car = cars[tableView.indexPathForSelectedRow!.row]
+        }
+    }
+
+}
